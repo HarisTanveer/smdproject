c09e9fa43c084a139da2e358333142dba5b37baa
Haris <haristanveer.ht@gmail.com>
2019-05-01 23:13:03 +0500

    Login try

--- Bagpackers/app/src/main/java/com/example/bagpackers/MainActivity.java
+++ Bagpackers/app/src/main/java/com/example/bagpackers/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.bagpackers;
 
-<<<<<<< HEAD
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
-=======
 import android.os.AsyncTask;
->>>>>>> d5420f510a49039efe3f6e0241a036b9373e742b
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -31,15 +28,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
         ActionBar actionbar=getSupportActionBar();
         actionbar.hide();
         p=(ImageView) findViewById(R.id.imageView2);
         final Animation a= AnimationUtils.loadAnimation(this,R.anim.fadein);
-=======
         p = (ImageView) findViewById(R.id.imageView2);
-        final Animation a = AnimationUtils.loadAnimation(this, R.anim.fadein);
->>>>>>> d5420f510a49039efe3f6e0241a036b9373e742b
         p.startAnimation(a);
 
         u=new User();
@@ -54,12 +47,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
 
-<<<<<<< HEAD
                 Intent intent =new Intent(MainActivity.this,login.class);
                 startActivity(intent);
-=======
                 new DataTask().execute();
->>>>>>> d5420f510a49039efe3f6e0241a036b9373e742b
+
 
             }
 
--- Bagpackers/app/src/main/java/com/example/bagpackers/login.java
+++ Bagpackers/app/src/main/java/com/example/bagpackers/login.java
@@ -1,16 +1,65 @@
 package com.example.bagpackers;
 
+import android.arch.persistence.room.Room;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.bagpackers.Classes.User;
+import com.example.bagpackers.RoomDB.AppDatabase;
 
 public class login extends AppCompatActivity {
 
+    private TextView email;
+    private TextView password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ActionBar actionbar=getSupportActionBar();
         actionbar.hide();
+        email=(TextView)findViewById(R.id.editText);
+        password=(TextView)findViewById(R.id.editText2);
+
     }
+
+    public void onLogin(View v)
+    {
+        new LoginTask().execute();
+    }
+
+
+    public class LoginTask extends AsyncTask<Void,Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+
+            AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "user").build();
+            User log=db.userDao().login(email.getText().toString(),password.getText().toString());
+            if(log!=null)
+                return true;
+            return false;
+
+        }
+
+        @Override
+        protected void onPostExecute(Boolean v) {
+            if(v)
+            {
+                Intent intent =new Intent(login.this,signup.class);
+                startActivity(intent);
+            }
+            else
+                {
+
+                    Toast.makeText(getApplicationContext(),"Invalid Username or Password",Toast.LENGTH_LONG);
+                }
+        }
+    }
+
 }
