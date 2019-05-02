1763532aaa8bfca06dc101d97547c5e86026f08e
Haris <haristanveer.ht@gmail.com>
2019-05-02 21:51:58 +0500

    Login Signup firebase done

--- Bagpackers/app/src/main/java/com/example/bagpackers/MainActivity.java
+++ Bagpackers/app/src/main/java/com/example/bagpackers/MainActivity.java
@@ -16,14 +16,6 @@ import android.widget.Toast;
 
 import com.example.bagpackers.Classes.User;
 import com.example.bagpackers.RoomDB.AppDatabase;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -42,38 +34,6 @@ public class MainActivity extends AppCompatActivity {
         final Animation a= AnimationUtils.loadAnimation(this,R.anim.fadein);
         p = (ImageView) findViewById(R.id.imageView2);
         p.startAnimation(a);
-
-        u=new User();
-        u.email="haristanveer.ht@gmail.com";
-        u.name="Haris Tanveer";
-        u.password="12345";
-        u.number="03468578686";
-
-
-
-        Map<String, Object> user = new HashMap<>();
-        user.put("Name", "Haris Tanveer");
-        user.put("email", "aristanveer.ht@gmail.com");
-        user.put("password", "12345");
-        user.put("number","03468578686");
-
-
-        //To insert in firebase
-//        FirebaseFirestore db = FirebaseFirestore.getInstance();
-//// Add a new document with a generated ID
-//        db.collection("user")
-//                .add(user)
-//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                    @Override
-//                    public void onSuccess(DocumentReference documentReference) {
-//                        Log.d("dasd", "DocumentSnapshot added with ID: " + documentReference.getId());
-//                    }
-//                }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//                Log.w("asd", "Error adding document", e);
-//            }
-//        });
 
 
         timer = new Timer();
--- Bagpackers/app/src/main/java/com/example/bagpackers/login.java
+++ Bagpackers/app/src/main/java/com/example/bagpackers/login.java
@@ -3,6 +3,7 @@ package com.example.bagpackers;
 import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,13 +12,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.bagpackers.Classes.User;
-import com.example.bagpackers.Firebase.FirebaseDB;
 import com.example.bagpackers.RoomDB.AppDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
 
 public class login extends AppCompatActivity {
 
     private TextView email;
     private TextView password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +39,27 @@ public class login extends AppCompatActivity {
 
     public void onLogin(View v)
     {
-        new LoginFirebase().execute();
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    Intent intent =new Intent(login.this,signup.class);
+                    startActivity(intent);
+
+                }
+
+            }
+        });
     }
 
+
+    public void toSignup(View v)
+    {
+        Intent intent =new Intent(login.this,signup.class);
+        startActivity(intent);
+    }
 
     //Local db se check kry ga using room
     public class LoginTask extends AsyncTask<Void,Void, Boolean> {
@@ -65,30 +91,13 @@ public class login extends AppCompatActivity {
     }
 
 
-    public class LoginFirebase extends AsyncTask<Void,Void,Boolean>
+    public class LoginFirebase extends AsyncTask<Void, Void, Void>
     {
 
         @Override
-        protected Boolean doInBackground(Void... voids) {
+        protected Void doInBackground(Void... voids) {
 
-            FirebaseDB db=FirebaseDB.getInstance();
-            boolean x=db.Login(email.getText().toString(),password.getText().toString());
-            return x;
-
-        }
-
-        @Override
-        protected void onPostExecute(Boolean b) {
-            if(b)
-            {
-                Intent intent =new Intent(login.this,signup.class);
-                startActivity(intent);
-            }
-            else
-            {
-
-                Toast.makeText(getApplicationContext(),"Invalid Username or Password",Toast.LENGTH_SHORT);
-            }
+            return null;
         }
     }
 
